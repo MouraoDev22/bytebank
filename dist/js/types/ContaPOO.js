52596afaa48e320cbdbbbaf45ced047dc4a29e3f
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-12 03:31:37 -0300

    inicializa classe Conta

---
+++ dist/js/types/ContaPOO.js
@@ -0,0 +1,17 @@
+class Conta {
+    nome;
+    saldo = JSON.parse(localStorage.getItem('saldo') || '0');
+    transacoes = JSON.parse(localStorage.getItem('transacoes') || '[]', (key, value) => {
+        if (key === 'data') {
+            return new Date(value);
+        }
+        ;
+        return value;
+    });
+    constructor(nome) {
+        this.nome = nome;
+    }
+    ;
+}
+;
+export default Conta;
